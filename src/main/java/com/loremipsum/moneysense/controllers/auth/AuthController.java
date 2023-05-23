853275aafe6aa86892cfa2c5c4607700718227a8
Orest Sonich <sonich.work@gmail.com>
2023-05-23 22:35:00 +0300

    add cross-origin

--- src/main/java/com/loremipsum/moneysense/controllers/auth/AuthController.java
+++ src/main/java/com/loremipsum/moneysense/controllers/auth/AuthController.java
@@ -3,14 +3,12 @@ package com.loremipsum.moneysense.controllers.auth;
 import com.loremipsum.moneysense.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
+@CrossOrigin
 public class AuthController {
 
     private final AuthService service;
